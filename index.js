6cda01aa0f73b58c242d26b5752c41728076ebbd
Jarad DeLorenzo <jaradd@gmail.com>
2014-12-07 13:31:54 -0500

    Changed listen port to 80

--- index.js
+++ index.js
@@ -52,6 +52,6 @@ io.on('connection', function(socket){
 	});
 });
 
-http.listen(3000, function(){
-	console.log('listening on *:3000');
+http.listen(80, function(){
+	console.log('listening on *:80');
 });
